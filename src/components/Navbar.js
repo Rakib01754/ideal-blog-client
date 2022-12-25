519b6857297f50208ab13365de6a231d4aefb03b
Rakib01754 <rakibul.freelancer567@gmail.com>
2022-12-25 17:08:25 +0600

    basic setup done

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import { Container } from '@mui/material';
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <Container>
+            Ideal Blogs
+        </Container>
+    );
+};
+
+export default Navbar;
